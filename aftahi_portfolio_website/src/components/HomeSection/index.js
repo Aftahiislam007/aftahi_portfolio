10a7e819fed50e097d66a66e257967a02093ba0a
Aftahiislam007 <aftahiislam2016@gmail.com>
2024-09-11 00:57:00 +0600

    navbar completed

---
+++ aftahi_portfolio_website/src/components/HomeSection/index.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const HomeContainer = styled.div`
+  background: ${({ theme }) => theme.card_light};
+  display: flex;
+  justify-content: center;
+  position: relative;
+  padding: 80px 30px;
+  @media (max-width: 960px) {
+    padding: 66px 16px;
+  }
+  @media (max-width: 640) {
+    padding: 32px 16px;
+  }
+  z-index: 1;
+
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 70% 95%, 0 100%);
+`;
+
+const HomeBg = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: end;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  max-width: 1360px;
+  overflow: hidden;
+  padding: 0 30px;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+
+  @media screen and (max-width: 960px) {
+    justify-content: center;
+    padding: 0 0px;
+  }
+`;
+
+const HomeInnerContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 1100px;
+
+  @media screen and (max-width: 960px) {
+    flex-direction: column;
+  }
+`;
+
+const HomeLeftContainer = styled.div`
+  width: 100%;
+  order: 1;
+  @media screen and (max-width: 960px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media screen and (max-width: 640px) {
+    order: 2;
+    margin-bottom: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const HomeRightContainer = styled.div`
+  width: 100%;
+  display: flex;
+  order: 2;
+  justify-content: end;
+  gap: 12px;
+  @media screen and (max-width: 960px) {
+    order: 1;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 80px;
+  }
+
+  @media screen and (max-width: 640px) {
+    margin-bottom: 30px;
+  }
+`;
+
+const Home = () => {
+  return (
+    <div id='about'>
+      <HomeContainer>
+        <HomeBg></HomeBg>
+        <HomeInnerContainer>
+          <HomeLeftContainer></HomeLeftContainer>
+          <HomeRightContainer></HomeRightContainer>
+        </HomeInnerContainer>
+      </HomeContainer>
+    </div>
+  )
+}
+
+export default Home
