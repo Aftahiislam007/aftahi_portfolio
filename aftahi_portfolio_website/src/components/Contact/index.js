15788c1a1bd614a19dbb87bd975fbfce36698647
Aftahiislam007 <aftahiislam2016@gmail.com>
2024-09-16 16:32:30 +0600

    contact section completed, background animation and about section working

--- aftahi_portfolio_website/src/components/Contact/index.js
+++ aftahi_portfolio_website/src/components/Contact/index.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { Snackbar } from '@mui/material';
+import { alignProperty } from '@mui/material/styles/cssUtils';
 
 const Container = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: center;
+justify-content: right;
 position: relative;
 z-index: 1;
 align-items: center;
@@ -17,19 +18,69 @@ align-items: center;
 `
 
 const Wrapper = styled.div`
-position: relative;
-display: flex;
-justify-content: space-between;
-align-items: center;
-flex-direction: column;
-width: 100%;
-max-width: 1350px;
-padding: 0px 0px 80px 0px;
-gap: 12px;
-@media (max-width: 960px) {
-    flex-direction: column;
-}
-`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  ${'' /* left: %; */}
+  width: 100%;
+  max-width: 1300px;
+  padding: 20px 30px 80px 0px;
+  gap: 18px;
+  @media (max-width: 960px) {
+      flex-direction: column;
+  }
+`;
+
+const Wrapper_div = styled.div`
+  ${'' /* position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  right: 30%;
+  width: 100%;
+  max-width: 1350px;
+  padding: 0px 0px 80px 0px;
+  gap: 10px;
+  @media (max-width: 960px) {
+      flex-direction: column;
+  } */}
+
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: 20px;
+  gap: 30px;
+  justify-content: left;
+`;
+
+const Skill = styled.div`
+  width: 100%;
+  max-width: 500px;
+  background: ${({ theme }) => theme.card};
+  border: 0.1px solid #854CE6;
+  box-shadow: rgba(23, 92, 230, 0.15) 0px 4px 24px;
+  border-radius: 16px;
+  padding: 18px 36px;
+  @media screen and (max-width: 768px) {
+    max-width: 400px;
+    padding: 10px 36px;
+  }
+  @media screen and (max-width: 500px) {
+    max-width: 330px;
+    padding: 10px 36px;
+  }
+`;
+
+const SkillTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.text_secondary};
+  margin-bottom: 20px;
+  text-align: center;
+`;
 
 const Title = styled.div`
 font-size: 42px;
@@ -120,7 +171,81 @@ const ContactButton = styled.input`
   font-weight: 600;
 `
 
+const Top = styled.div`
+    width: 100%;
+    display: flex;
+    gap: 12px
+`;
 
+const Body = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column; 
+`;
+
+
+const Name = styled.div`
+    font-size: 18px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text_primary + 99};
+    @media only screen and (max-width: 768px){
+        font-size: 14px;
+    }
+`;
+
+const Degree = styled.div`
+    font-size: 14px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.text_secondary + 99};
+    @media only screen and (max-width: 768px){
+        font-size: 12px;
+    }
+`;
+
+const Date = styled.div`
+    font-size: 12px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.text_secondary + 80};
+    @media only screen and (max-width: 768px){
+        font-size: 10px;
+    }
+`;
+
+export const ResumeButton = styled.a`
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
+    text-decoration: none;
+    width: 95%;
+    max-width: 300px;
+    text-align: center;
+    padding: 16px 0;
+    color:${({ theme }) => theme.white};
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 20px;
+    font-weight: 600;
+    transition: all 0.2s ease-in-out !important;
+    background: hsla(271, 100%, 50%, 1);
+    background: linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    background: -moz-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    background: -webkit-linear-gradient(225deg, hsla(271, 100%, 50%, 1) 0%, hsla(294, 100%, 50%, 1) 100%);
+    box-shadow:  20px 20px 60px #1F2634,
+    -20px -20px 60px #1F2634;
+    &:hover {
+        transform: scale(1.05);
+    transition: all 0.4s ease-in-out;
+    box-shadow:  20px 20px 60px #1F2634,
+    filter: brightness(1);
+    }    
+    
+    
+    @media (max-width: 640px) {
+        padding: 12px 0;
+        font-size: 18px;
+    } 
+
+`;
 
 const Contact = () => {
 
@@ -143,9 +268,42 @@ const Contact = () => {
 
   return (
     <Container id="contact">
-      <Wrapper>
-        <Title>Contact</Title>
-        <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+      <Title>Contact</Title>
+      <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
+      <Wrapper> 
+        <Skill>
+          <SkillTitle>Email</SkillTitle>
+          <Top>
+            <Body>
+                <Name>aftahiislam2016@gmail.com</Name><br />
+                {/* <Degree>bachelor</Degree>
+                <Date>8th sep 2021</Date> */}
+            </Body>
+          </Top>
+          <ResumeButton href="mailto:aftahiislam2016@gmail.com" target='display'>Send Mail</ResumeButton>
+        </Skill>
+        <Skill>
+          <SkillTitle>WhatsApp</SkillTitle>
+          <Top>
+            <Body>
+                <Name>+8801797341164</Name><br />
+                {/* <Degree>bachelor</Degree>
+                <Date>8th sep 2021</Date> */}
+            </Body>
+          </Top>
+          <ResumeButton href="https://wa.me/qr/VLDC5HGQP5EDG1" target='display'>Send Message</ResumeButton>
+        </Skill>
+        <Skill>
+          <SkillTitle>Mobile</SkillTitle>
+          <Top>
+            <Body>
+                <Name>+8801797341164</Name><br />
+                {/* <Degree>bachelor</Degree>
+                <Date>8th sep 2021</Date> */}
+            </Body>
+          </Top>
+          <ResumeButton href="tel:+8801797341164" target='display'>Call Now</ResumeButton>
+        </Skill>
         <ContactForm ref={form} onSubmit={handleSubmit}>
           <ContactTitle>Email Me 🚀</ContactTitle>
           <ContactInput placeholder="Your Email" name="from_email" />
@@ -162,6 +320,12 @@ const Contact = () => {
           severity="success"
         />
       </Wrapper>
+      {/* <Wrapper_div>
+        <Skill>
+          <SkillTitle>Email</SkillTitle>
+          
+        </Skill>
+      </Wrapper_div> */}
     </Container>
   )
 }
